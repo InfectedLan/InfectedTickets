e04419933bab613a364348358cc86aa258b47009
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-07-18 01:35:08 +0200

    Fixed brutal formatting error

--- v2/pages/tickets.php
+++ v2/pages/tickets.php
@@ -19,68 +19,67 @@ class Page
 				self::printTicket($tikkit);
 			}
 		}
-
-		public function renderTutorial()
-		{
-			echo 'Yolo';
-		}
-		
-		private function printTicket($ticket)
-		{
-			//The code
-			$seater = $ticket->getSeater();
-			echo '<table>';
-				echo '<tr>';
-					echo '<td>';
-						echo '<b>' . $ticket->getHumanName() . '</b>';
-					echo '</td>';
-					echo '<td>';
-						echo '<input type="button" value="Overfør biletten" onclick="searchUser(\'Overfør billetten!\', function(user) { $.getJSON(\'http://' . $_SERVER['HTTP_HOST'] . '/api/json/transferTicket.php?id=' . $ticket->getId() . '&target=\' + user, handleJson); })" />';
-					echo '</td>';
-					echo '<td>';
-						echo '<input type="button" value="Endre plassreserverer" /><br />';
-					echo '</td>';
-					echo '<td>';
-						echo '<input type="button" value="Skriv ut Billett" />';
-					echo '</td>';
-					echo '<td>';
-						echo '<input type="button" value="Lagre mobil versjon" />';
-					echo '</td>';
-				echo '</tr>';
-				//Seater row :P
-				echo '<tr>';
-					echo '<td></td>'; //name
-					echo '<td></td>'; //transfer
-					echo '<td>'; //seater
-						echo '<center>';
-							if(!isset($sete))
-							{
-								echo "(Deg)";
-							}
-							else
-							{
-								echo '(' . $seater->getDisplayName() . ')';
-							}
-						echo '</center>';
-					echo '</td>';
-					echo '<td></td>'; //print
-					echo '<td></td>'; //mobile
-				echo '</tr>';
-			echo '</table>';
-			echo '<hr />';
-			/*
-			echo '<div class="ticket_div">';
-				echo '<b>' . $ticket->getHumanName() . '</b>';
-				if(!isset($sete))
-				{
-					echo '<b>Ikke plassert</b>';
-				}
-				echo '<br />';
-				echo 'Eier: <b>' . $ticket->getOwner()->getDisplayName() . '</b><br />';
-				echo 'Seater: <b>' . $ticket->getSeater()->getDisplayName() . '</b><br />';
-			echo '</div>';
-			*/
-		}
+	}
+	public function renderTutorial()
+	{
+		echo 'Yolo';
+	}
+	
+	private function printTicket($ticket)
+	{
+		//The code
+		$seater = $ticket->getSeater();
+		echo '<table>';
+			echo '<tr>';
+				echo '<td>';
+					echo '<b>' . $ticket->getHumanName() . '</b>';
+				echo '</td>';
+				echo '<td>';
+					echo '<input type="button" value="Overfør biletten" onclick="searchUser(\'Overfør billetten!\', function(user) { $.getJSON(\'http://' . $_SERVER['HTTP_HOST'] . '/api/json/transferTicket.php?id=' . $ticket->getId() . '&target=\' + user, handleJson); })" />';
+				echo '</td>';
+				echo '<td>';
+					echo '<input type="button" value="Endre plassreserverer" onclick="searchUser(\'Sett som seater!\', function(user) { $.getJSON(\'http://' . $_SERVER['HTTP_HOST'] . '/api/json/setSeater.php?id=' . $ticket->getId() . '&target=\' + user, handleJson); })" /><br />';
+				echo '</td>';
+				echo '<td>';
+					echo '<input type="button" value="Skriv ut Billett" />';
+				echo '</td>';
+				echo '<td>';
+					echo '<input type="button" value="Lagre mobil versjon" />';
+				echo '</td>';
+			echo '</tr>';
+			//Seater row :P
+			echo '<tr>';
+				echo '<td></td>'; //name
+				echo '<td></td>'; //transfer
+				echo '<td>'; //seater
+					echo '<center>';
+						if(!isset($seater))
+						{
+							echo "(Deg)";
+						}
+						else
+						{
+							echo '(' . $seater->getDisplayName() . ')';
+						}
+					echo '</center>';
+				echo '</td>';
+				echo '<td></td>'; //print
+				echo '<td></td>'; //mobile
+			echo '</tr>';
+		echo '</table>';
+		echo '<hr />';
+		/*
+		echo '<div class="ticket_div">';
+			echo '<b>' . $ticket->getHumanName() . '</b>';
+			if(!isset($sete))
+			{
+				echo '<b>Ikke plassert</b>';
+			}
+			echo '<br />';
+			echo 'Eier: <b>' . $ticket->getOwner()->getDisplayName() . '</b><br />';
+			echo 'Seater: <b>' . $ticket->getSeater()->getDisplayName() . '</b><br />';
+		echo '</div>';
+		*/
 	}
 }
 ?>
