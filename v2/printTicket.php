0b04466caa5ac2cc25725d1328833b39856d4a95
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-07-22 00:28:50 +0200

    Work on ticket printer

--- v2/printTicket.php
+++ v2/printTicket.php
@@ -1,3 +1,50 @@
 <?php
-	echo '<h1>Dette skal bli noe fett</h1>';
+	require_once 'utils.php';
+	require_once 'handlers/tickethandler.php';
+
+	echo '<html>';
+		echo '<head>';
+			echo '<title>Biletten din</title>';
+			echo '<link rel="stylesheet" type="text/css" href="style/ticket.css">';
+		echo '</head>';
+		echo '<body>';
+			//Validation
+			if(!isset($_GET["id"]))
+			{
+				echo 'Ugyldig bilett-id</body></html>';
+				return;
+			}
+			$ticket = TicketHandler::getTicket($_GET["id"]);
+			if(!isset($ticket))
+			{
+				echo 'Biletten eksisterer ikke</body></html>';
+				return;
+			}
+			echo '<div id="total">';
+				echo '<h2>Billett: ' . $ticket->getHumanName() . '</h2>';
+				echo '<table>';
+					echo '<tr><td width="100px"><b>Navn:</b></td><td>' . $ticket->getOwner()->getFullName() . '</td></tr>';
+					echo '<tr><td>Født:</td><td>' . date("j. F o", $ticket->getOwner()->getBirthdate()) .'</td></tr>';
+					echo '<tr><td>Adresse:</td><td>' . $ticket->getOwner()->getAddress() . '</td></tr>';
+					echo '<tr><td>Mobil:</td><td>' . $ticket->getOwner()->getPhone() . '</td></tr>';
+					echo '<tr><td>Brukernavn:</td><td>' . $ticket->getOwner()->getUsername() . '</td></tr>';
+					if(!isset($ticket->getSeat()))
+					{
+						echo '<tr><td>Sete:</td><td><b>IKKE PLASSERT</b></td></tr>';
+					}
+					else
+					{
+						echo '<tr><td>Sete:</td><td>' . $ticket->getSeat()->getHumanString() . '</td></tr>';
+					}
+					echo '<tr><td colspan="2"><b>Innsjekking: <a href="https://github.com/InfectedLan/InfectedAPI/issues/27">Please poke this issue</a></b></td></tr>';
+					echo '<div id="inputdata"><img src="' . $ticket->getQr() . '" width="200px"/></div>';
+				echo '</table>';
+			echo '</div>';
+			echo '<div id="tekstprint">Denne billetten skal vises ved innsjekking på Radar. Husk å ta med gyldig legitimasjon. De under 14 må ha med
+bekreftelse fra foreldre. Skjema på nettsiden.</div>';
+			echo '<div id="logo">'
+				echo '<img src="images/logo_infected.jpg" width="299px">';
+			echo '</div>';
+		echo '</body>';
+	echo '</html>';
 ?>
