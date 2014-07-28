a111fa78309ea0611b95e3cc3ae15b55620b81e4
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-07-28 23:31:24 +0200

    fix to support JSONparse or something like that

--- v2/scripts/splash.php
+++ v2/scripts/splash.php
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		e.preventDefault();
 	    $.post( '../json/login.php', $('#loginForm').serialize(), function(data) { // TODO: Link this to api in a more elegant way.
 	    	console.log('Got data!');
-	        if(data.result=="true")
+	        if(data.result==true)
 	        {
 	        	location.reload();
 	        }
