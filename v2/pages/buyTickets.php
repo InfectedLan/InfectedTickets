b00461f7a94fd88984796263293497426b4facdf
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-08-12 01:56:24 +0200

    Work on ticket buying stuff

--- v2/pages/buyTickets.php
+++ v2/pages/buyTickets.php
@@ -23,7 +23,7 @@ class Page {
 	public function renderFirstStep() {
 		$currentEvent = EventHandler::getCurrentEvent();
 		$type = TicketTypeHandler::getTicketType( $currentEvent->getTicketType() );
-		echo '<h1>Kjøper bilett for ' . 
+		echo '<h1>Kjøper bilett for ' . $currentEvent->getTheme() . '</h1>'; 
 	}
 }
 ?>
