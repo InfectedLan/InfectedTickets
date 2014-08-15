09d64e0bc77e37dc15a8e7a87967b48a61f251b4
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-15 13:44:32 +0200

    Fixed login brake caused by API change

--- v2/scripts/splash.php
+++ v2/scripts/splash.php
@@ -1,42 +1,33 @@
 //Login form submitter
 $(document).ready(function() {
-	$('#loginForm').submit( function(e) {
+	$('#loginForm').submit(function(e) {
 		e.preventDefault();
-	    $.post( '../json/login.php', $('#loginForm').serialize(), function(data) { // TODO: Link this to api in a more elegant way.
-	    	console.log('Got data!');
-	        if(data.result==true)
-	        {
-	        	location.reload();
-	        }
-	        else
-	        {
-	         	error(data.message);
-	        }
-	       },
-	       'json'
-	    );
+		$.getJSON('../json/login.php' + '?' + $(this).serialize(), function(data){
+			if (data.result) {
+				location.reload();
+			} else {
+				error(data.message); 
+			}
+		});
 	});
+	
 	$('#registerForm').submit( function(e) {
 		e.preventDefault();
-	    $.post( '../json/register.php', $('#registerForm').serialize(), function(data) { // TODO: Link this to api in a more elegant way.
-	    	console.log('Got data!');
-	        if(data.result==true)
-	        {
-	        	//$('#registerForm').reset();
-	        	showLoginBoxFromRegister();
+		$.getJSON('../json/register.php' + '?' + $(this).serialize(), function(data){
+			if (data.result) {
+				showLoginBoxFromRegister();
 	        	info("Din bruker har blitt laget! Sjekk e-posten din for å aktivere, før du logger inn.");
-
-	        }
-	        else
-	        {
-	         	error(data.message);
-	        }
-	       },
-	       'json'
-	    );
+			} else {
+				error(data.message); 
+			}
+		});
 	});
-	$('.errorClose').click(function() {hideErrorBox();});
+	
+	$('.errorClose').click(function() {
+		hideErrorBox();
+	});
 });
+
 //View faders
 function showRegisterBox() {
 	$("#loginFrame").fadeOut(200, function() {
