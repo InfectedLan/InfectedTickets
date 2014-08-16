d744759ad34a566ee692f4aff81a4bf2758f4c31
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-16 11:05:03 +0200

    Forces user to access the page over https

--- v2/index.php
+++ v2/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'site.php';
 
+// Force user to use https.
+if (!isset($_SERVER['HTTPS']) || 
+	$_SERVER['HTTPS'] != 'on') {
+	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+}
+
 // Execute the site.
 $site = new Site();
 $site->execute();
