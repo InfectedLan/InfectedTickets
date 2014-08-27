a059c1586812340d9f2b5a2bae1c67724b9ff004
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-27 23:54:45 +0200

    Added google analytics

--- v2/site.php
+++ v2/site.php
@@ -55,6 +55,15 @@ class Site {
 				echo '<script src="../api/scripts/jquery.js"></script>';
 				echo '<script src="../api/scripts/jquery.form.min.js"></script>';
 				echo '<script src="scripts/shared.js"></script>';
+				echo '<script>';
+					echo '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
+					echo '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
+					echo 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
+					echo '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');';
+
+					echo 'ga(\'create\', \'UA-54254513-2\', \'auto\');';
+					echo 'ga(\'send\', \'pageview\');';
+				echo '</script>';	
 			echo '</head>';
 			echo '<body>';
 				if (Session::isAuthenticated()) {
