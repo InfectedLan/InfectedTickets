cc6ac936a5005eaac1f558c062eee0260975ac38
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-28 17:09:43 +0200

    Fixed registration code bug

--- v2/pages/activation.php
+++ v2/pages/activation.php
@@ -2,14 +2,22 @@
 require_once 'handlers/registrationcodehandler.php';
 
 if (isset($_GET['code'])) {
-	RegistrationCodeHandler::removeRegistrationCode($_GET['code']);
-	
-	echo '<li>';
-		echo '<p>Brukeren din er nå aktivert og klar for bruk.</p>';
-	echo '</li>';
+	$code = $_GET['code'];
+
+	if (RegistrationCodeHandler::hasRegistrationCode($code)) {
+		RegistrationCodeHandler::removeRegistrationCode($_GET['code']);
+		
+		echo '<li>';
+			echo '<p>Brukeren din er nå aktivert og klar for bruk.</p>';
+		echo '</li>';
+	} else {
+		echo '<li>';
+			echo '<p>Brukeren din er allerede aktivert.</p>';
+		echo '</li>';
+	}
 } else {
 	echo '<li>';
-		echo '<p>Brukeren din er allerede aktivert.</p>';
+		echo '<p>En feil oppstod.</p>';
 	echo '</li>';
 }
 ?>
