058fc09bf845309f6d575b1bbc3618a472e54468
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-12 09:50:26 +0100

    Fixed forced https.

--- v2/index.php
+++ v2/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'site.php';
 
-// Force user to use https.
-if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
+// Force user to use https if we are not in a development environment. Seriously, i wont want to spend 1 hour setting up my dev environment because one line cannot allow me non-ssl.
+if ((!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') && $_SERVER['HTTP_HOST'] != 'localhost') {
 	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 }
 
