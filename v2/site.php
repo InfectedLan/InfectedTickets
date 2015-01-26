2e4cc60d0e9f06ec9904f93b62203fba6eb06cac
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-26 12:15:14 +0100

    Compatible with login API changes.

--- v2/site.php
+++ v2/site.php
@@ -173,7 +173,7 @@ class Site {
 										echo '<div id="loginFrame">';
 											echo '<form class="login" method="post">';
 												echo '<li>';
-													echo '<input class="input" type="text" name="username" placeholder="Brukernavn / epost">';
+													echo '<input class="input" type="text" name="identifier" placeholder="Brukernavn eller E-post">';
 												echo '</li>';
 												echo '<li>';
 													echo '<input class="input" name="password" type="password" placeholder="Passord">';
