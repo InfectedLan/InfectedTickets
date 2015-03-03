c9a4d4d57eecf93a84cba27656d2d60429b8feea
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-03 23:48:19 +0100

    Added GPL header.

--- v2/printTicket.php
+++ v2/printTicket.php
@@ -1,74 +1,93 @@
 <?php
-	require_once 'session.php';
-	require_once 'settings.php';
-	require_once 'handlers/tickethandler.php';
+/**
+ * This file is part of InfectedTickets.
+ *
+ * Copyright (C) 2015 Infected <http://infected.no/>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-	echo '<html>';
-		echo '<head>';
-			echo '<title>Billetten din</title>';
-			echo '<link rel="stylesheet" href="styles/ticket.css">';
-			echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
-		echo '</head>';
-		
-		if (isset($_GET['print'])) {
-			echo '<body onload="window.print()">';
-		} else {
-			echo '<body>';
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'session.php';
+require_once 'settings.php';
+require_once 'handlers/tickethandler.php';
+
+echo '<html>';
+	echo '<head>';
+		echo '<title>Billetten din</title>';
+		echo '<link rel="stylesheet" href="styles/ticket.css">';
+		echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
+	echo '</head>';
+	
+	if (isset($_GET['print'])) {
+		echo '<body onload="window.print()">';
+	} else {
+		echo '<body>';
+	}
+		//Validation
+		if(!isset($_GET["id"])) {
+			echo 'Ugyldig billett-id</body></html>';
+			return;
 		}
-			//Validation
-			if(!isset($_GET["id"])) {
-				echo 'Ugyldig billett-id</body></html>';
-				return;
-			}
+		
+		$ticket = TicketHandler::getTicket($_GET["id"]);
+		
+		if(!isset($ticket)) {
+			echo 'Billetten eksisterer ikke</body></html>';
 			
-			$ticket = TicketHandler::getTicket($_GET["id"]);
+			return;
+		}
+		
+		if(!Session::isAuthenticated()) {
+			echo 'Du er ikke logget inn!</body></html>';
 			
-			if(!isset($ticket)) {
-				echo 'Billetten eksisterer ikke</body></html>';
-				
-				return;
-			}
-			
-			if(!Session::isAuthenticated()) {
-				echo 'Du er ikke logget inn!</body></html>';
-				
-				return;
-			}
-			
-			if (!Session::getCurrentUser()->equals($ticket->getUser())) {
-				echo 'Du eier ikke denne billetten!</body></html>';
-				return;
-			}
+			return;
+		}
+		
+		if (!Session::getCurrentUser()->equals($ticket->getUser())) {
+			echo 'Du eier ikke denne billetten!</body></html>';
+			return;
+		}
 
-			echo '<div id="total">';
-				echo '<h2>Billett: ' . $ticket->getString() . '</h2>';
-				echo '<table>';
-					echo '<tr><td width="100px"><b>Navn:</b></td><td>' . $ticket->getUser()->getFullName() . '</td></tr>';
-					echo '<tr><td>Født:</td><td>' . date('d.m.Y', $ticket->getUser()->getBirthdate()) .'</td></tr>';
-					echo '<tr><td>Adresse:</td><td>' . $ticket->getUser()->getAddress() . '</td></tr>';
-					echo '<tr><td>Mobil:</td><td>' . $ticket->getUser()->getPhoneAsString() . '</td></tr>';
-					echo '<tr><td>Brukernavn:</td><td>' . $ticket->getUser()->getUsername() . '</td></tr>';
-					$seat = $ticket->getSeat();
-					if( !isset($seat) )
-					{
-						echo '<tr><td>Sete:</td><td><b>IKKE PLASSERT</b></td></tr>';
-						echo '<tr><td colspan="2"><b>Innsjekking: IKKE PLASSERT</b></td></tr>';
-					}
-					else
-					{
-						$entrance = $seat->getRow()->getEntrance();
-						echo '<tr><td>Sete:</td><td>' . $seat->getString() . '</td></tr>';
-						echo '<tr><td colspan="2"><b>Innsjekking: ' . $entrance->getTitle() . '</b></td></tr>';
-					}
-					
-					echo '<div id="inputdata"><img src="' . "" . '../api/content/qrcache/' . $ticket->getQrImagePath() . '" width="200px"/></div>';
-				echo '</table>';
-			echo '</div>';
-			echo '<div id="tekstprint">Denne billetten skal vises ved innsjekking på Radar. Husk å ta med gyldig legitimasjon. De under 14 må ha med
+		echo '<div id="total">';
+			echo '<h2>Billett: ' . $ticket->getString() . '</h2>';
+			echo '<table>';
+				echo '<tr><td width="100px"><b>Navn:</b></td><td>' . $ticket->getUser()->getFullName() . '</td></tr>';
+				echo '<tr><td>Født:</td><td>' . date('d.m.Y', $ticket->getUser()->getBirthdate()) .'</td></tr>';
+				echo '<tr><td>Adresse:</td><td>' . $ticket->getUser()->getAddress() . '</td></tr>';
+				echo '<tr><td>Mobil:</td><td>' . $ticket->getUser()->getPhoneAsString() . '</td></tr>';
+				echo '<tr><td>Brukernavn:</td><td>' . $ticket->getUser()->getUsername() . '</td></tr>';
+				$seat = $ticket->getSeat();
+				if( !isset($seat) )
+				{
+					echo '<tr><td>Sete:</td><td><b>IKKE PLASSERT</b></td></tr>';
+					echo '<tr><td colspan="2"><b>Innsjekking: IKKE PLASSERT</b></td></tr>';
+				}
+				else
+				{
+					$entrance = $seat->getRow()->getEntrance();
+					echo '<tr><td>Sete:</td><td>' . $seat->getString() . '</td></tr>';
+					echo '<tr><td colspan="2"><b>Innsjekking: ' . $entrance->getTitle() . '</b></td></tr>';
+				}
+				
+				echo '<div id="inputdata"><img src="' . "" . '../api/content/qrcache/' . $ticket->getQrImagePath() . '" width="200px"/></div>';
+			echo '</table>';
+		echo '</div>';
+		echo '<div id="tekstprint">Denne billetten skal vises ved innsjekking på Radar. Husk å ta med gyldig legitimasjon. De under 14 må ha med
 bekreftelse fra foreldre og <a href="http://infected.no/v7/files/Foreldreskjema.doc">dette</a> skjema.</div>';
-			echo '<div id="logo">';
-				echo '<img src="images/logo_infected.jpg" width="299px">';
-			echo '</div>';
-		echo '</body>';
-	echo '</html>';
+		echo '<div id="logo">';
+			echo '<img src="images/logo_infected.jpg" width="299px">';
+		echo '</div>';
+	echo '</body>';
+echo '</html>';
 ?>
