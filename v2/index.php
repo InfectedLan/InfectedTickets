b75157019a25dc960d2a1607b1ac695719b08028
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-17 13:24:31 +0100

    We're doing this in Apache now.

--- v2/index.php
+++ v2/index.php
@@ -20,11 +20,6 @@
 
 require_once 'site.php';
 
-// Force user to use https if we are not in a development environment. Seriously, i wont want to spend 1 hour setting up my dev environment because one line cannot allow me non-ssl.
-if ((!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') && $_SERVER['HTTP_HOST'] != 'localhost') {
-	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-}
-
 // Execute the site.
 $site = new Site();
 $site->execute();
